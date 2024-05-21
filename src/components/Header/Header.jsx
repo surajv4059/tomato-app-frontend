9f919e218e9162dda180e45e16ecf6ec379085a5
Suraj Verma <suraj12.116@gmail.com>
2024-05-22 02:29:08 +0530

    added functionality to view menu button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className="header">
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delecatable arrray of dishes crafted with the finest ingredients and culinary expertise. Our meal will satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
-            <button>View Menu</button>
+            <Link to='/'><button >View Menu</button></Link>
         </div>
     </div>
   )
